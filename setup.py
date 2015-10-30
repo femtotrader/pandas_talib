044c16b86e93d59cf94baa5c591424b55a1078c0
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2015-10-30 14:08:07 -0300

    remove matplotlib from requirements since it is not imported

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pandas', 'matplotlib'],
+    install_requires=['pandas'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
